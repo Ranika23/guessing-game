ed6814cc4e3908ae272d8bec7934b72a97f079eb
Ranika23 <bveronikal@gmail.com>
2023-10-29 14:36:39 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNumber = Math.round((this.max - this.min) / 2 + this.min);
+        return this.guessNumber;
     }
 
     lower() {
-
+            this.max = this.guessNumber;
     }
 
     greater() {
-
-    }
+            this.min = this.guessNumber;
+        }
 }
 
 module.exports = GuessingGame;
